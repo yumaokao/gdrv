420a2cc9677b3486c3542dec50c1692d3a4bbd36
yumaokao <yumaokao@mint>
2013-08-12 13:25:03 +0800

    [YMK] Add main.py.

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+
+import sys
+import argparse
+import re
+
+
+def main():
+    print("YMK Goodbye World!!!")
+
+if __name__ == '__main__':
+    main()
