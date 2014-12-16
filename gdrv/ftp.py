f9713be566a00c6e9454de9699fc0b655e7dd1cb
yumaokao <ymkq9h@gmail.com>
2014-12-16 20:39:20 +0800

    [YMK] prepare dynamically added method to cmd in ftp mode

--- gdrv/ftp.py
+++ gdrv/ftp.py
@@ -5,6 +5,14 @@
 from cmd import Cmd
 
 
+def real_do_gdrv(line):
+    print("YMK do_gdrv")
+
+
+def real_complete_gdrv(text, line, begidx, endidx):
+    return ["yumaokao", "yumao.kao", "yumaokao74", "ymk74"]
+
+
 class DriveFtp(Cmd):
     """ Google drive interactive mode """
 
@@ -12,9 +20,20 @@ class DriveFtp(Cmd):
         self.commands = commands
         self.config = config
         Cmd.__init__(self)
+        setattr(self, 'do_gdrv', real_do_gdrv)
+        setattr(self, 'complete_gdrv', real_complete_gdrv)
+
+#    def completedefault(self, text, line, begidx, endidx):
+#        return ["ymk", "exit", "gdrv"]
+
+    def get_names(self):
+        return ["do_ymk", "do_exit", "do_gdrv", "do_help"]
 
     def do_ymk(self, line):
-        print("YMK FTP")
+        print("YMK FTP get_names() {0}".format(self.get_names()))
+
+    def complete_ymk(self, text, line, begidx, endidx):
+        return ["yumaokao", "yumao.kao", "yumaokao74", "ymk74"]
 
     def do_exit(self, line):
         return True
